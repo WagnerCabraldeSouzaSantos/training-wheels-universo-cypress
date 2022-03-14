8a9cd3ce9b6efba76e3bbee416d3547b87770d7e
Wagner Cabral <wcdss12@gmail.com>
2022-03-13 22:04:05 -0300

    Atualização da estratégia de seleção de mês no datepicker

--- cypress/integration/datepicker.spec.js
+++ cypress/integration/datepicker.spec.js
@@ -2,15 +2,14 @@ it('selecionar a data de nascimento', function(){
     cy.visit('/datepicker')
 
     const date = {
-        month: 'mar',
+        month: 'Mar',
         year: '1993',
         day: '28'
     }
 
     cy.get('.datetimepicker-dummy-input').click()
     cy.get('.datepicker-nav-month').click()
-    cy.get('div[data-month="03"]').click()
-    //cy.contains('.datepicker-month', 'date.month').click()
+    cy.contains('.datepicker-month', date.month).click()
 
     cy.get('.datepicker-nav-year').click()
     cy.contains('.datepicker-year span', date.year).click()
